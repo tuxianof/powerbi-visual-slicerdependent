b870172d1e65cc88bae1b7f3416bf7b3cfef5bff
tuxianof <tuxianof@gmail.com>
2025-05-06 06:08:28 -0500

    Initial commit

---
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var slicerdependent43D0BDDB1F1C41158F68DD73E8C65330_DEBUG: IVisualPlugin;
+export default slicerdependent43D0BDDB1F1C41158F68DD73E8C65330_DEBUG;
